7328511f73c651efd254f1d49027e4da3e132983
Christophe Alladoum <85187342+calladoum-elastic@users.noreply.github.com>
2025-11-23 16:32:06 -0800

    fixes

--- ttd/src/replay/events.rs
+++ ttd/src/replay/events.rs
@@ -48,12 +48,12 @@ impl From<u8> for EventType {
 }
 
 impl TryFrom<u8> for EventType {
-    fn try_from(value: u8) -> Result<Self>{
-        let obj = Self { value };
+    fn try_from(value: u8) -> Result<EventType>{
+        let obj = EventType::from(value);
         if obj.value == EventType::Invalid {
             return Err(Error::ConversionError);
         }
-        
+
         Ok(obj)
     }
 }
