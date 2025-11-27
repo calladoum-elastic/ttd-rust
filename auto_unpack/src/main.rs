20749c5970e44acb1562ef423852d9bb2f468ffd
Christophe Alladoum <christophe.alladoum@elastic.co>
2025-11-27 12:06:41 -0800

    auto_unpack - added mock redline  in memory yara rule

--- auto_unpack/src/main.rs
+++ auto_unpack/src/main.rs
@@ -2,18 +2,25 @@
 // or encrypted in the trace. We achieve this by watching VirtualProtect
 // calls with the PAGE_EXECUTE_READWRITE.
 // For each of those calls, we jump ahead to the time the culprit address
-// is being executed, and we print the disassembled payload.
+// is being executed, print the disassembled payload and Yara-scan it.
 //
+use std::io::{Read, Write};
 
 use anyhow::{Ok, Result, bail};
 use extfmt::{AsHexdump, hexdump};
-use log::{debug, info, warn};
+use log::{debug, info};
 
 use ttd::replay::events::{DataAccessMask, EventType};
 use ttd::replay::{MemoryWatchpointData, RegisterContext, ReplayEngine, ReplayPosition};
 
+const NUMBER_OF_INSTRUCTIONS_TO_PRINT: usize = 20;
+
 fn main() -> Result<()> {
-    env_logger::Builder::new().filter_level(log::LevelFilter::max()).init();
+    #[cfg(debug_assertions)]
+    env_logger::Builder::new().filter_level(log::LevelFilter::Debug).init();
+    #[cfg(not(debug_assertions))]
+    env_logger::Builder::new().filter_level(log::LevelFilter::Info).init();
+
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         bail!("No trace provided");
@@ -45,19 +52,19 @@ fn main() -> Result<()> {
         }
     };
 
-    debug!("looking for kernelbase!VirtualProtect()");
-    let virtualprotect_address = {
-        let event = find_load_event_for_module("kernelbase.dll")?;
+    let get_module_base_address = |module_name: &str, function_name: &str| -> Result<u64> {
+        let event = find_load_event_for_module(module_name)?;
         let mut cur = replay.cursor()?;
         cur.set_position(&(ReplayPosition::from(&event.position)));
         let data = cur.read_memory(event.module.address, event.module.size as usize)?;
         debug!("data@{:#x}\n{}", event.module.address, hexdump!(&data[..64]));
-        let rva = find_export(&data, "VirtualProtect")? as u64;
+        let rva = find_export(&data, function_name)? as u64;
         debug!("rva={:#x}", rva);
-        event.module.address + rva
+        Ok(event.module.address + rva)
     };
 
-    assert_eq!(virtualprotect_address, 0x77124290);
+    debug!("looking for kernelbase!VirtualProtect()");
+    let virtualprotect_address = get_module_base_address("kernelbase.dll", "VirtualProtect")?;
     info!("Watching calls to kernelbase!VirtualProtect(RWX) (at {:#x})", virtualprotect_address);
 
     //
@@ -75,15 +82,31 @@ fn main() -> Result<()> {
     }
     info!("Watching for execution at {:#x} set", virtualprotect_address);
 
-    let read_u64_at = |addr: u32, pos: &ReplayPosition| -> Result<u32> {
+    let read_u64_at = |addr: u64, pos: &ReplayPosition| -> Result<u32> {
         let mut _cur = replay.cursor()?;
         _cur.set_position(pos);
-        let data = _cur.read_memory(addr.into(), _cur.pointer_size()?)?;
-        Ok(u32::from_le_bytes(data.to_owned().try_into().unwrap()))
+        let data: [u8; 4] = _cur
+            .read_memory(addr.into(), _cur.pointer_size()?)?
+            .try_into()
+            .expect("Slice with incorrect length");
+        Ok(u32::from_le_bytes(data))
     };
 
     let fmt = zydis::Formatter::intel();
-    let dec = zydis::Decoder::new32();
+    let dec = {
+        match cursor.thread_context()? {
+            RegisterContext::X86(_) => zydis::Decoder::new32(),
+            RegisterContext::X64(_) => zydis::Decoder::new64(),
+            RegisterContext::ARM64(_) => todo!("use bad64"),
+        }
+    };
+
+    let mut yara_compiler = yara_x::Compiler::new();
+    let mut source: Vec<u8> = vec![];
+    std::fs::File::open(r"C:\git\ttd\auto_unpack\rules\redline.yar")?.read_to_end(&mut source)?;
+    yara_compiler.add_source(&*source)?;
+    let yara_rules = yara_compiler.build();
+    let mut yara_scanner = yara_x::Scanner::new(&yara_rules);
 
     loop {
         //
@@ -95,23 +118,35 @@ fn main() -> Result<()> {
         }
         debug!("fwdreplay reason: {}, executed {} insns", result.stop_reason, result.instructions_executed);
 
-        let ctx = cursor.thread_context()?;
-        let curpos = cursor.position()?;
-        debug!("context at {}:\n{}", &curpos, &ctx);
-
         //
         // 4. Inspect the `flNewProtect` argument, looking PAGE_EXECUTE_READWRITE
         //
         const PAGE_EXECUTE_READWRITE: u32 = 0x40;
 
-        let (arg0, arg1, arg2, arg3) = match ctx {
-            RegisterContext::X86(x86_ctx) => (
-                read_u64_at(x86_ctx.Esp + 0x04, &curpos)?,
-                read_u64_at(x86_ctx.Esp + 0x08, &curpos)?,
-                read_u64_at(x86_ctx.Esp + 0x0c, &curpos)?,
-                read_u64_at(x86_ctx.Esp + 0x10, &curpos)?,
-            ),
-            _ => unimplemented!(),
+        let (arg0, arg1, arg2, arg3) = {
+            let ctx = cursor.thread_context()?;
+            let curpos = cursor.position()?;
+            debug!("context at {}:\n{}", &curpos, &ctx);
+            match ctx {
+                RegisterContext::X86(x86_ctx) => (
+                    read_u64_at((x86_ctx.Esp + 0x04) as u64, &curpos)?,
+                    read_u64_at((x86_ctx.Esp + 0x08) as u64, &curpos)?,
+                    read_u64_at((x86_ctx.Esp + 0x0c) as u64, &curpos)?,
+                    read_u64_at((x86_ctx.Esp + 0x10) as u64, &curpos)?,
+                ),
+                RegisterContext::X64(x64_ctx) => (
+                    read_u64_at(x64_ctx.Rcx, &curpos)?,
+                    read_u64_at(x64_ctx.Rdx, &curpos)?,
+                    read_u64_at(x64_ctx.R8, &curpos)?,
+                    read_u64_at(x64_ctx.R9, &curpos)?,
+                ),
+                RegisterContext::ARM64(arm64_ctx) => (
+                    read_u64_at(arm64_ctx.X[0], &curpos)?,
+                    read_u64_at(arm64_ctx.X[1], &curpos)?,
+                    read_u64_at(arm64_ctx.X[2], &curpos)?,
+                    read_u64_at(arm64_ctx.X[3], &curpos)?,
+                ),
+            }
         };
 
         if arg2 != PAGE_EXECUTE_READWRITE {
@@ -119,7 +154,7 @@ fn main() -> Result<()> {
             continue;
         }
 
-        info!("kernelbase!VirtualProtect({arg0:x}, {arg1:x}, {arg2:x}, {arg3:x}) found, setting watch point");
+        info!("kernelbase!VirtualProtect({arg0:x}, {arg1:x}, {arg2:x}, {arg3:x}) hit, setting watch point");
 
         //
         // 5. Set an execution watchpoint on execution to that address
@@ -135,26 +170,55 @@ fn main() -> Result<()> {
 
         cursor.replay_forward(None)?;
 
-        let ctx = match cursor.thread_context()? {
-            RegisterContext::X86(ctx) => ctx,
-            _ => unimplemented!(),
+        let pc = match cursor.thread_context()? {
+            RegisterContext::X86(ctx) => ctx.Eip as u64,
+            RegisterContext::X64(ctx) => ctx.Rip as u64,
+            RegisterContext::ARM64(ctx) => ctx.Pc as u64,
         };
-        let mem = cursor.read_memory(ctx.Eip.into(), arg1 as usize)?;
+        let mem = cursor.read_memory(pc, arg1 as usize)?;
 
         //
-        // 6. When the watchpoint is hit, dump all the instructions
+        // 6. When the watchpoint is hit, analyze the shellcode
         //
-        info!("Dumping shellcode at IP={:x} (sz={})", ctx.Eip, arg1);
-        let decoder_iter = dec.decode_all::<zydis::VisibleOperands>(&mem, ctx.Eip.into());
-        for (idx, insn_info) in decoder_iter.enumerate() {
-            let insn_info = insn_info?;
-            let ip = insn_info.0;
-            let insn = &insn_info.2;
-            println!("{:#08x} {}", ip, fmt.format(Some(ip), insn)?);
-            if idx >= 20 {
-                println!("...");
-                warn!("Skipping following instructions...");
-                break;
+
+        //
+        // 6.0. Dump the payload to disk for later analysis
+        //
+        {
+            let mut tempfile = std::path::PathBuf::from(std::env::var("TEMP")?);
+            tempfile.push(format!("Payload-{}.dmp", cursor.position()?.to_string().replace(":", "_")));
+            std::fs::File::create(tempfile)?.write_all(&mem)?;
+        }
+
+        //
+        // 6.1. Disassemble the instructions
+        //
+        {
+            info!("Dumping shellcode at IP={:x} (sz={}) executed at {}", pc, arg1, cursor.position()?);
+            let decoder_iter = dec.decode_all::<zydis::VisibleOperands>(&mem, pc);
+            for (idx, insn_info) in decoder_iter.enumerate() {
+                if idx == NUMBER_OF_INSTRUCTIONS_TO_PRINT {
+                    println!("...");
+                    break;
+                }
+
+                let insn_info = insn_info?;
+                let ip = insn_info.0;
+                let insn = &insn_info.2;
+                println!("{:#08x} {}", ip, fmt.format(Some(ip), insn)?);
+            }
+        }
+
+        //
+        // 6.2. Launch a Yara scan
+        //
+        {
+            let results = yara_scanner.scan(&mem)?;
+            if results.matching_rules().len() > 0 {
+                info!("Yara hit:");
+                results.matching_rules().for_each(|rule| info!("- {}", rule.identifier()));
+            } else {
+                debug!("No Yara hit");
             }
         }
 
